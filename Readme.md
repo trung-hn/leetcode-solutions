3822a99cc15f6062fa69a2b935294e7c9a7a98c1
Trung Hoang <trung.nang.hoang@gmail.com>
2020-05-07 03:28:52 -0400

    Add 993

--- Readme.md
+++ Readme.md
@@ -4,12 +4,13 @@ Medium: 1
 Hard: 0
 |No.|Problem|Level|Time|Space|
 |---|---|---|---|---|
-|1    |Two Sum | Easy | O(N) | O(N) |
-|46   |Permutations | Medium |  |   |
-|387  | First Unique Character in a String | Easy | O(N) | O(N) |
-|476  | Number Complement | Easy | O(N) | O(1) |
-|830  | Positions of Large Groups | Easy | O(N) | O(1) |
-|     |   |   |   |   |
-|     |   |   |   |   |
+|1 | Two Sum | Easy | O(N) | O(N) |
+|46 | Permutations | Medium | Pending | Pending |
+|387 | First Unique Character in a String | Easy | O(N) | O(N) |
+|476 | Number Complement | Easy | O(N) | O(1) |
+|830 | Positions of Large Groups | Easy | O(N) | O(1) |
+|993 | Cousins in Binary Tree | Easy | O(N) | O(H) |
+| | | | | |
+| | | | | |
 
 Total: 568 problems. 
---
+++ src/993.cousins-in-binary-tree.py
@@ -0,0 +1,35 @@
+#
+# @lc app=leetcode id=993 lang=python3
+#
+# [993] Cousins in Binary Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# TAGS: trees, recursion
+class Solution:
+    # 24 ms, 97.22%. Time: O(N). Space: O(H)
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        self.rv = False
+        def tree_traversal(root, depth=0):
+            if not root: return 0
+            if root.val == x or root.val == y: return depth
+            left = tree_traversal(root.left, depth + 1)
+            right = tree_traversal(root.right, depth + 1)
+
+            # Check if depth are equals and not direct children
+            if left == right and left > depth + 1:
+                self.rv = True
+            
+            # Return max child
+            return max(left, right)
+        tree_traversal(root)
+        return self.rv
+
+# @lc code=end
+
