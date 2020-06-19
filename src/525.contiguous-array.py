50d28a840f7851cf586706b90be9acf112f95ff5
Trung Hoang <trung.nang.hoang@gmail.com>
2020-06-18 20:45:17 -0400

    Update 525

--- src/525.contiguous-array.py
+++ src/525.contiguous-array.py
@@ -8,10 +8,18 @@
 # TAGS: Hash Table
 # REVIEWME: Very good problem. Think graph
 class Solution:
-    """
-    In both solution, Time is O(N) and Space is O(N)
-    """
-    # 872 ms, 95.30%
+    # TLE. Time: O(N^3). Space: O(1)
+    def findMaxLength(self, nums: List[int]) -> int:
+        L = len(nums)
+        if L % 2: L -= 1
+        for length in range(L, 0, -2): # O(N)
+            for i in range(len(nums) - length + 1): # O(N)
+                ones = sum(nums[i : i + length]) # O(N)
+                if ones == length // 2:
+                    return length
+        return 0
+
+    # 872 ms, 95.30%. Time: O(N), Space: O(N)
     def findMaxLength(self, nums: List[int]) -> int:
         count = max_len = 0 
         D = {0:0}
@@ -22,7 +30,8 @@ class Solution:
             else:
                 D[count] = i
         return max_len
-    # 924 ms, 63.58%
+
+    # 924 ms, 63.58%. Pythonic. Time: O(N), Space: O(N)
     def findMaxLength(self, nums: List[int]) -> int:
         count = max_len = 0 
         D = {0:0}
@@ -30,6 +39,7 @@ class Solution:
             count += 1 if num else -1
             max_len = max(max_len, i - D.setdefault(count, i))
         return max_len
+     
      
 # @lc code=end
 
