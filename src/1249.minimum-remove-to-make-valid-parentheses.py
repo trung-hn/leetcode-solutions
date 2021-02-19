37ca3471df75b9a5b45cc37a7ba118b4d57c4a1e
Trung Hoang <trung.nang.hoang@gmail.com>
2021-02-19 18:31:22 -0500

    Update 1249

--- src/1249.minimum-remove-to-make-valid-parentheses.py
+++ src/1249.minimum-remove-to-make-valid-parentheses.py
@@ -17,16 +17,25 @@ class Solution:
                 op += 1
                 close_parens.append(i)
             elif c == ")":
+                if op == 0:
+                    to_remove.add(i)
+                    continue
                 op -= 1
-            if op < 0:
-                to_remove.add(i)
-                op += 1
-        if op:
-            for val in close_parens[-op:]:
-                to_remove.add(val)
-
+        if op: to_remove |= set(close_parens[-op:])
         return "".join(c for i, c in enumerate(s) if i not in to_remove)
-         
+
+    # Same idea but slightly cleaner         
+    def minRemoveToMakeValid(self, s: str) -> str:
+        s = list(s)
+        stack = []
+        for i, c in enumerate(s):
+            if c == "(":
+                stack.append(i)
+            elif c == ")":
+                if stack: stack.pop()
+                else: s[i] = ""
+        for i in stack: s[i] = ""
+        return "".join(s)
 
 # @lc code=end
 
