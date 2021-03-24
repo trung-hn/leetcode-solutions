bc43f8f86933e58b93d7f54590e260318e6876fc
Trung Hoang <trung.nang.hoang@gmail.com>
2021-03-24 13:45:35 -0400

    Update 870

--- src/870.advantage-shuffle.py
+++ src/870.advantage-shuffle.py
@@ -6,12 +6,15 @@
 
 # @lc code=start
 # TAGS: Array, Greedy
+import collections
+
+
 class Solution:
-    # 368 ms, 49.27%. Time: O(NlogN) and Space: O(N)
+    # 368 ms, 49.27%. First Solution Time: O(NlogN) and Space: O(N)
     def advantageCount(self, A: List[int], B: List[int]) -> List[int]:
         a = list(reversed(sorted(A)))
         b = list(reversed(sorted(B)))
-        
+
         # Find As that can be used to cover Bs
         relation = collections.defaultdict(list)
         i = j = 0
@@ -20,7 +23,7 @@ class Solution:
                 relation[b[j]].append(a[i])
                 i += 1
             j += 1
-        
+
         # Create the rv array
         ans = []
         for val in B:
@@ -30,5 +33,32 @@ class Solution:
                 ans.append(a[i])
                 i += 1
         return ans
-# @lc code=end
 
+    # 344 ms, 88.09%.  Second solution. Time: O(NlogN). Space: O(N)
+    def advantageCount(self, A: List[int], B: List[int]) -> List[int]:
+        A = sorted(A)
+        B = sorted((val, i) for i, val in enumerate(B))
+        pre = []
+        post = []
+        j = 0
+        for a in A:
+            if a > B[j][0]:
+                pre.append(a)
+                j += 1
+            else:
+                post.append(a)
+        # Reconstruct A
+        for a, (_, i) in zip(pre + post, B):
+            A[i] = a
+        return A
+
+    # Same complexity but cleaner from lee215
+
+    def advantageCount(self, A, B):
+        A = sorted(A)
+        take = collections.defaultdict(list)
+        for b in sorted(B)[::-1]:
+            if b < A[-1]:
+                take[b].append(A.pop())
+        return [(take[b] or A).pop() for b in B]
+# @lc code=end
