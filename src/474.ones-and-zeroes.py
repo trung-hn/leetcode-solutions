e1774c62ac1ec7124688a8cc2431a7c913c3488c
Trung Hoang <trung.nang.hoang@gmail.com>
2021-04-02 23:01:24 -0400

    Update 474

--- src/474.ones-and-zeroes.py
+++ src/474.ones-and-zeroes.py
@@ -8,25 +8,66 @@
 # TAGS: Dynamic Programming
 # REVIEWME: Dynamic Programming
 import collections
+from functools import cache
+
+
 class Solution:
-    # 2844 ms, 82.28%. Time: O(K*M*N). Space: O(K*M*N)
+    # 2252 ms, 90.27%. Time: O(N*M*L). Space: O(N*M*L)
     # Space complexity can be improved with bottom up dp (rolling array)
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        visited = {}
-        def dfs(i=0, zeroes=0, ones=0, cnt=0):
-            # Memoization
-            if (i, zeroes, ones) in visited and visited[(i, zeroes, ones)] >= cnt: 
+        @cache
+        def dp(i, ones, zeroes):
+            if i == len(strs):
                 return 0
-            visited[(i, zeroes, ones)] = cnt
-            
-            if zeroes > m or ones > n: return cnt - 1
-            if i == len(strs): return cnt
-            take = dfs(i + 1, zeroes + freqs[i][0], ones + freqs[i][1], cnt + 1)
-            skip = dfs(i + 1, zeroes, ones, cnt)
+            if ones < 0 or zeroes < 0:
+                return 0
+
+            # Skip this str
+            skip = dp(i + 1, ones, zeroes)
+
+            # One, zero of current str
+            one, zero = counters[i]["1"], counters[i]["0"]
+            if one > ones or zero > zeroes:
+                return skip
+
+            # Take this str
+            take = dp(i + 1, ones - one, zeroes - zero) + 1
             return max(take, skip)
+        counters = [collections.Counter(s) for s in strs]
+        return dp(0, n, m)
 
-        freqs = [collections.Counter(s) for s in strs]
-        freqs = [(c["0"], c["1"])for c in freqs]
-        return dfs()
+    # 4004 ms, 32.59%
+    def findMaxForm(sezlf, strs: List[str], m: int, n: int) -> int:
+        def count(s):
+            return sum(1 for c in s if c == '0'), sum(1 for c in s if c == '1')
+        prev = [[0] * (n + 1) for _ in range(m + 1)]
+        for z, o in [count(s) for s in strs]:
+            cur = []
+            for x in range(m + 1):
+                row = []
+                for y in range(n + 1):
+                    if x >= z and y >= o:
+                        row.append(max(1 + prev[x - z][y - o], prev[x][y]))
+                    else:
+                        row.append(prev[x][y])
+                cur.append(row)
+            prev = cur
+        return prev[m][n]
+
+    # Bottom-up DP, Rolling Array. From discussion
+    # Time: O(N*M*L). Space: O(N*M)
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        def count(s):
+            return sum(1 for c in s if c == '0'), sum(1 for c in s if c == '1')
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for z, o in [count(s) for s in strs]:
+            for x in range(m, -1, -1):
+                for y in range(n, -1, -1):
+                    if x >= z and y >= o:
+                        dp[x][y] = max(1 + dp[x-z][y-o], dp[x][y])
+
+        return dp[m][n]
+
+
 # @lc code=end
-
