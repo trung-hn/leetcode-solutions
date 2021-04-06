a93fe3029a916a1b73185048f103240a27dbd5f9
Trung Hoang <trung.nang.hoang@gmail.com>
2021-04-06 12:21:08 -0400

    Update 735

--- src/735.asteroid-collision.py
+++ src/735.asteroid-collision.py
@@ -6,20 +6,42 @@
 
 # @lc code=start
 # TAGS: Stack
+
+
 class Solution:
-    # Time and Space: O(N)
+    # Pythonic
+    # 108 ms, 26.73%. Time and Space: O(N).
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         stack = []
         for asteroid in asteroids:
-            if asteroid < 0:
-                while stack and 0 < stack[-1] < -asteroid:
-                    stack.pop()
-                if not stack or stack[-1] < 0:
-                    stack.append(asteroid)
-                elif stack[-1] == -asteroid:
-                    stack.pop()
+            while stack and asteroid < 0 < stack[-1]:
+                prev = stack.pop()
+
+                # Both got destroyed
+                if prev + asteroid == 0:
+                    break
+
+                # Else get the bigger one by size
+                asteroid = max(asteroid, prev, key=abs)
             else:
                 stack.append(asteroid)
         return stack
-# @lc code=end
 
+    # Any language
+    # 96 ms, 73.31%. Time and Space: O(N)
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for asteroid in asteroids:
+            while stack and asteroid < 0 and 0 < stack[-1]:
+                prev = stack.pop()
+
+                # Both got destroyed
+                if abs(asteroid) == abs(prev):
+                    asteroid = 0
+                # The biggest one by size
+                elif abs(asteroid) < abs(prev):
+                    asteroid = prev
+            if asteroid:
+                stack.append(asteroid)
+        return stack
+# @lc code=end
