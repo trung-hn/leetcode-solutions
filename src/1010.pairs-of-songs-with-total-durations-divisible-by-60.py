284584857b5efa9af9213177abea395a00c9b705
Trung Hoang <trung.nang.hoang@gmail.com>
2021-04-16 09:36:43 -0400

    Update 1010

--- src/1010.pairs-of-songs-with-total-durations-divisible-by-60.py
+++ src/1010.pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -6,10 +6,20 @@
 
 # @lc code=start
 # TAGS: Array, Math
+# REVIEWME: Math
+
+
 class Solution:
+    """
+    Approach:
+    First sol: try all combinations that are % 60
+    Second sol: because this is modulo,
+    we can keep track of only val % 60
+    """
     # 452 ms, 5.02%. Time and Space: O(N)
+
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        divisible_by_60 = list(range(60, 1001, 60))
+        divisible_by_60 = list(range(60, 501, 60))
         ans = 0
         visited = collections.defaultdict(int)
         for t in time:
@@ -17,11 +27,11 @@ class Solution:
                 ans += visited[d - t]
             visited[t] += 1
         return ans
-    
-    # 228 ms, 54.62%. Time: O(N) Space: O(1)
+
+    # 236 ms, 47.49%. Time: O(N) Space: O(1)
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
         ans = 0
-        visited = collections.defaultdict(int)
+        visited = [0] * 60
         for t in time:
             if t % 60 == 0:
                 ans += visited[0]
@@ -30,4 +40,3 @@ class Solution:
             visited[t % 60] += 1
         return ans
 # @lc code=end
-
