594142406bcf449ef02e049d4ccd4e9d2f2f8c34
Trung Hoang <trung.nang.hoang@gmail.com>
2021-05-28 21:23:03 -0400

    Update 1695

--- src/1695.maximum-erasure-value.py
+++ src/1695.maximum-erasure-value.py
@@ -6,6 +6,8 @@
 
 # @lc code=start
 # TAGS: Two Pointers.
+
+
 class Solution:
     # 1216 ms, 91.76%. Time: O(N). Space: O(1)
     def maximumUniqueSubarray(self, nums: List[int]) -> int:
@@ -21,5 +23,21 @@ class Solution:
             ans = max(ans, total)
         return ans
 
+    # Second sollution after a while.
+    # 1328 ms, 48.07%. Time: O(N). Space: O(1)
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        counter = collections.defaultdict(int)
+        ans = curr = start = 0
+        for num in nums:
+            # Update counter and total
+            counter[num] += 1
+            curr += num
+            # While counter of current number > 1
+            while counter[num] > 1:
+                # Update counter, curr and move start
+                counter[nums[start]] -= 1
+                curr -= nums[start]
+                start += 1
+            ans = max(ans, curr)
+        return ans
 # @lc code=end
-
