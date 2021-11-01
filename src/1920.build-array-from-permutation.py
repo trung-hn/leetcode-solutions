fb12b249f5248196d2dde7438c567ba991c4bdb8
Trung Hoang <trung.nang.hoang@gmail.com>
2021-10-31 22:39:36 -0400

    Addd 1920

---
+++ src/1920.build-array-from-permutation.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=1920 lang=python3
+#
+# [1920] Build Array from Permutation
+#
+
+# @lc code=start
+
+
+class Solution:
+    def buildArray(self, nums: List[int]) -> List[int]:
+        return [nums[i] for i in nums]
+# @lc code=end
