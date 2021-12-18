1393be0fac81e4ca8f2a7acf9535a7c514ce277b
Trung Hoang <trung.nang.hoang@gmail.com>
2021-12-18 17:52:37 -0500

    Updated 934 for youtube

--- src/934.shortest-bridge.py
+++ src/934.shortest-bridge.py
@@ -6,61 +6,52 @@
 
 # @lc code=start
 # TAGS: BFS, DFS
+from typing import List
+
+
 class Solution:
+    # 404 ms, 70.53%. BFS. Time and Space: O(N*N)
+    def shortestBridge(self, grid: List[List[int]]) -> int:
 
-    # Helper function used by both
-    def change_island_to_minus_1(self, A):
-        def dfs(r, c):
-            A[r][c] = -1
-            for x, y in (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1):
-                if  0 <= x < R and 0 <= y < C and A[x][y] == 1:
-                    dfs(x, y)
-                    
-        R, C = len(A), len(A[0])
-        for r in range(R):
-            for c in range(C):
-                if A[r][c] == 1:
-                    dfs(r, c)
-                    return A
+        # Mark 1 island as 2
+        r, c = self.find_first_land(grid)
+        self.mark_island_as_2(grid, r, c)
 
-    # 3352 ms, 5.02%. DFS
-    def shortestBridge(self, A: List[List[int]]) -> int:
-        def dfs(r, c, depth=-1):
-            if depth >= depth_matrix[r][c]: return
-            depth_matrix[r][c] = depth
-            # if depth >= self.ans: return
-            if A[r][c] == 1:
-                self.ans = min(self.ans, depth)
-                return
+        # Get all lands of island 1
+        N = len(grid)
+        island_1_lands = set((r, c)
+                             for r in range(N)
+                             for c in range(N)
+                             if grid[r][c] == 1)
 
-            for x, y in (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1):
-                if  0 <= x < R and 0 <= y < C and A[x][y] != -1:
-                    dfs(x, y, depth + 1)
-        
-        self.ans = float("inf")
-        R, C = len(A), len(A[0])
-        depth_matrix = [[R] * R for _ in range(R)]
-        A = self.change_island_to_minus_1(A)
-        for r in range(R):
-            for c in range(C):
-                if A[r][c] == -1:
-                    dfs(r, c)
-        return self.ans
-        
-    # 416 ms, 60.08%. BFS. Pay attention to how we improve times by 10 fold just by changing A "1 depth earlier."
-    def shortestBridge(self, A: List[List[int]]) -> int:
-        R, C = len(A), len(A[0])
-        A = self.change_island_to_minus_1(A)
-        q = collections.deque([(r, c, 0) for r in range(R) for c in range(C) if A[r][c] == -1])
-        while q:
-            r, c, depth = q.popleft()
-            A[r][c] = -1
-            for x, y in (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1):
-                if  0 <= x < R and 0 <= y < C:
-                    if A[x][y] == 1:
-                        return depth
-                    elif A[x][y] == 0:
-                        A[x][y] = -1 # although we do this in the next step, remove this will make it extremely slow.
-                        q.append((x, y, depth + 1))
+        # Find shortest bridge
+        dist = 0
+        while island_1_lands:
+            new_island_1_lands = set()
+            for r, c in island_1_lands:
+                for x, y in (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1):
+                    # Not valid
+                    if not(0 <= x < N and 0 <= y < N):
+                        continue
+
+                    # Flip water to bridge
+                    if grid[x][y] == 0:
+                        grid[x][y] = - dist - 1
+                        new_island_1_lands.add((x, y))
+                    elif grid[x][y] == 2:
+                        return dist
+            dist += 1
+            island_1_lands = new_island_1_lands
+
+    def find_first_land(self, grid):
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    return (r, c)
+
+    def mark_island_as_2(self, grid, r, c):
+        grid[r][c] = 2
+        for x, y in (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1):
+            if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == 1:
+                self.mark_island_as_2(grid, x, y)
 # @lc code=end
-
