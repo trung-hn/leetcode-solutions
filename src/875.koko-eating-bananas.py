165e7ef13ab5d20bfa41c573ab89339b7a78e0e3
Trung Hoang <trung.nang.hoang@gmail.com>
2022-07-10 19:17:19 -0400

    Updated 875

--- src/875.koko-eating-bananas.py
+++ src/875.koko-eating-bananas.py
@@ -6,21 +6,26 @@
 
 # @lc code=start
 # TAGS: Binary Search
+from typing import List
+
+
 class Solution:
-    # 540 ms, 59.53%. Time: O(NlogN). Space: O(1)
+    # 540 ms, 59.53%. Time: O(NlogM). Space: O(1). N is len(nums), M is max(nums)
     def minEatingSpeed(self, piles: List[int], H: int) -> int:
-        if len(piles) == H: return max(piles)
-        
+        if len(piles) == H:
+            return max(piles)
+
         def can_finish(K, H):
             total = 0
             for pile in piles:
                 total += pile // K + bool(pile % K)
             return total <= H
-        
+
         lo, hi = 1, max(piles)
-        # This will make it 4 times faster:
-        # lo, hi = math.ceil(total / H) , math.ceil(tot / (H-len(piles)+1))
-        
+        # Alternatively, we can init lo and hi as follows (4 times faster)
+        # total = sum(piles)
+        # lo, hi = math.ceil(total / H) , math.ceil(total / (H-len(piles)+1))
+
         while lo < hi:
             mid = (lo + hi) // 2
             if can_finish(mid, H):
@@ -28,5 +33,6 @@ class Solution:
             else:
                 lo = mid + 1
         return lo
-# @lc code=end
 
+
+# @lc code=end
