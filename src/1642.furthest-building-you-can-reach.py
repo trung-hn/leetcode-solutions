a217ad81f73c376ba91533d7cbf751457262b5dc
Trung Hoang <trung.nang.hoang@gmail.com>
2024-02-17 17:46:08 +0000

    Updated 1642

--- src/1642.furthest-building-you-can-reach.py
+++ src/1642.furthest-building-you-can-reach.py
@@ -6,20 +6,23 @@
 
 # @lc code=start
 # TAGS: Binary Search, Heap
+import heapq
+
+
 class Solution:
-    # 580 ms, 45.4%. Time: O(NlogN). Space: O(N)
+    # 412 ms, 86.22%. Time: O(NlogN). Space: O(N)
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
         heap = []
-        cnt = 0
+        ans = 0
         for b1, b2 in zip(heights, heights[1:]):
             if b1 < b2:
                 heapq.heappush(heap, b2 - b1)
-                delta_h = 0
                 if len(heap) > ladders:
-                    delta_h = heapq.heappop(heap)
-                bricks -= delta_h
-                if bricks < 0: break
-            cnt += 1
-        return cnt
-# @lc code=end
+                    bricks -= heapq.heappop(heap)
+                if bricks < 0:
+                    break
+            ans += 1
+        return ans
 
+
+# @lc code=end
