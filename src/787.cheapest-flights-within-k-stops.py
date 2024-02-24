ae4eb390d7e9710e13c1411d63537c223bdd2f0d
Trung Hoang <trung.nang.hoang@gmail.com>
2024-02-24 02:20:28 +0000

    Updated 787

--- src/787.cheapest-flights-within-k-stops.py
+++ src/787.cheapest-flights-within-k-stops.py
@@ -6,40 +6,42 @@
 
 # @lc code=start
 # TAGS: Dynamic Programming, Heap, Breadth First Search:
-# REVIEWME:  Dijkstra's Algorithm, Graph, 
+# REVIEWME:  Dijkstra's Algorithm, Graph
+import collections
+from typing import List
+import heapq
+
+
 class Solution:
     """
-    Read article for more info about complexity and other solutions
+    # Idea: Use a min heap to find the fatest way to get to node (not cheapest)
     """
-    # 104 ms, 69.88%.
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
-        graphs = collections.defaultdict(list)
-        for s, d, w in flights:
-            graphs[s].append([d, w])
-        
-        dist_tbl = [float("inf")]*n
-        stops_tbl = [float("inf")]*n
-        min_heap = [[0, 0, src]]
-        while min_heap:
-            weight, k, dest = heapq.heappop(min_heap)
-            
-            if dest == dst:
-                break
 
-            if k == K + 1:
+    # 98 ms, 64.80%.
+    # Time: O((E+V)*log(E+V)). Space: O(N*K)
+    def findCheapestPrice(
+        self, n: int, flights: List[List[int]], src: int, dst: int, k: int
+    ) -> int:
+        fs = collections.defaultdict(list)
+        for s, d, p in flights:
+            fs[s].append((d, p))
+
+        visited = collections.defaultdict(lambda: float("inf"))
+        heap = [(0, 0, src)]
+        while heap:
+            stops, cost, curr = heapq.heappop(heap)
+
+            # Optimal solution found for `curr` already
+            if curr in visited and visited[curr] <= cost:
                 continue
-            
-            for nxt_dst, next_w in graphs[dest]:
-                if next_w + weight < dist_tbl[nxt_dst]:
-                    dist_tbl[nxt_dst] = next_w + weight
-                    heapq.heappush(min_heap, [next_w + weight, k + 1, nxt_dst])
+            visited[curr] = min(visited[curr], cost)
 
-                elif k < stops_tbl[nxt_dst]:
-                    # Also consider case with longer distance but less stops. 
-                    stops_tbl[nxt_dst] = next_w + weight
-                    heapq.heappush(min_heap, [next_w + weight, k + 1, nxt_dst])
+            if stops > k:
+                continue
+            for d, p in fs[curr]:
+                heapq.heappush(heap, (stops + 1, cost + p, d))
 
-        return dist_tbl[dst] if dist_tbl[dst] != float("inf") else -1
-            
+        return visited.get(dst, -1)
+
+
 # @lc code=end
-
