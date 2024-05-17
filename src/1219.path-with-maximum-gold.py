2acd169113527ed57b5f925a4d45e2476eccb465
Trung Hoang <trung.nang.hoang@gmail.com>
2024-05-18 00:48:17 +0100

    Edit 1219

--- src/1219.path-with-maximum-gold.py
+++ src/1219.path-with-maximum-gold.py
@@ -4,23 +4,25 @@
 # [1219] Path with Maximum Gold
 #
 
+
 # @lc code=start
 # TAGS: Backtracking
 class Solution:
     # 1248 ms, 73.98%. Time: O(R*C+N*2^N) where N is cell with gold (max=25). Space: O(N)
     def getMaximumGold(self, grid: List[List[int]]) -> int:
-        def dfs(r, c, visited=set()):
+        def dfs(r, c):
             rv = 0
-            visited.add((r, c))
+            gold, grid[r][c] = grid[r][c], 0
             for x, y in (r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1):
-                if 0 <= x < R and 0 <= y < C and grid[x][y] and (x, y) not in visited:
-                    rv = max(rv, dfs(x, y, visited))
-            visited.discard((r, c))
-            return rv + grid[r][c]
-        
-        R, C = len(grid), len(grid[0])
-        cells_with_gold = [(r, c) for r in range(R) for c in range(C) if grid[r][c]]
-        return max(dfs(r, c) for r, c in cells_with_gold) if cells_with_gold else 0
-                
-# @lc code=end
+                if 0 <= x < R and 0 <= y < C and grid[x][y] and grid[x][y]:
+                    rv = max(rv, dfs(x, y))
+            grid[r][c] = gold
+            return rv + gold
 
+        R, C = len(grid), len(grid[0])
+        return max(
+            [dfs(r, c) for r in range(R) for c in range(C) if grid[r][c]], default=0
+        )
+
+
+# @lc code=end
