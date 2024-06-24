7c820f687c42baea73d0cf4e779c30579cfb8104
Trung Hoang <trung.nang.hoang@gmail.com>
2024-06-24 01:24:02 +0100

    Added 997

--- src/997.find-the-town-judge.py
+++ src/997.find-the-town-judge.py
@@ -10,12 +10,31 @@ from typing import List
 
 
 class Solution:
-    # 805 ms, 20.73%. Time and Space: O(N)
+    # 1576 ms, 5.04%. Time: O(N*M). Space: O(1)
     def findJudge(self, N: int, trust: List[List[int]]) -> int:
-        trust_freq = [0] * (N + 1)
-        for a, b in trust:
-            trust_freq[a] -= 1
-            trust_freq[b] += 1
-        judge = [i for i, val in enumerate(trust_freq) if val == N - 1]
-        return judge.pop() if judge else - 1
+
+        # Edge case
+        if not trust and N == 1:
+            return N
+
+        for person in range(1, N + 1):  # O(N)
+            out_trust = sum(a == person for a, _ in trust)  # O(M)
+            in_trust = sum(b == person for _, b in trust)  # O(M)
+            if out_trust == 0 and in_trust == N - 1:
+                return person
+        return -1
+
+    # 1576 ms, 5.04%. Time: O(N + M). Space: O(N)
+    def findJudge1(self, N: int, trust: List[List[int]]) -> int:
+
+        trust_cnt = [0] * (N + 1)
+        for a, b in trust:  # O(M)
+            trust_cnt[a] -= 1
+            trust_cnt[b] += 1
+        for i in range(1, N + 1):  # O(N)
+            if trust_cnt[i] == N - 1:
+                return i
+        return -1
+
+
 # @lc code=end
